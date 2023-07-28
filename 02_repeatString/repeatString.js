ba0897f578c9df890153349af7687decae803ebc
PavanDNaik <pawannaik396@gmail.com>
2023-07-28 23:22:49 +0530

    1st and 2nd done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string ,num) {
+    let ret = '';
+    while(num > 0)
+    {
+        ret+=string;
+        num--;
+    }
+    // console.log(string);
+    return (num < 0 )?'ERROR':ret;
+    
 };
 
 // Do not edit below this line
