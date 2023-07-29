1386632ceac7fa2226942e8eaaa4fc66939d1c95
PavanDNaik <pawannaik396@gmail.com>
2023-07-29 13:19:31 +0530

    9 and 10 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let text = string.toLowerCase();
+    let str = text.replace(/[^a-z0-9]/g,'');
+    let rev = str.split("").reverse().join('');
+    if(rev === str)return true;
+    return false;
+    // let n= string.length;
+    // string = string.toUpperCase();
+    // let l=0,r=string.length -1;
+    // while(true){
+    //     while(l < n && (/[A-Z]/).test(string.charAt(l))) l++;
+    //     while(r > 0 && (/[A-Z]/).test(string.charAt(r))) r--;
+    //     if(l >= r || l>=n || r<0 )break;
+    //     else if(string.charAt(l) !== string.charAt(r))return false;
+    //     r--;
+    //     l++;
+    // }
+    // return true;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if( n < 0 )return "OOPS";
+    let arr=[1,1];
+    for(let l=2;l<n;l++){
+        arr[l]=arr[l-1]+arr[l-2];
+    }
+    return arr[n-1];
 };
 
 // Do not edit below this line
