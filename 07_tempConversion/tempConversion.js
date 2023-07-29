c4c7b3fba16ee0b2e33fa769722bf3dcc0a618a5
PavanDNaik <pawannaik396@gmail.com>
2023-07-29 12:41:45 +0530

    6 7 and 8 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let C = (F - 32) * 5/9;
+  return Math.round(C * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let F = (C * 9/5 + 32);
+  return Math.round(F * 10)/10;
 };
 
 // Do not edit below this line
@@ -9,3 +13,9 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+ 
+/*                
+
+Fahrenheit	x °C ≘ (x × 9/5+ 32) °F	
+x °F ≘ (x − 32) × 5/9 °C
+*/
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum=0;
+  for(const c of arr){
+    sum += c;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mul=1;
+  for(const c of arr){
+    mul *= c;
+  }
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(x , n) {
+  return x ** n;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if( n == 0 || n == 1)return 1;
+
+    let fact = 1;
+    for(let i=2;i<=n;i++){
+      fact *=i;
+    }
+    return fact;
+
 };
 
 // Do not edit below this line
